ab6e7a982ab080d34b7c6188751ac95ccba8d979
Alexandr Toropov <bligs100@gmail.com>
2022-10-29 13:41:42 +0600

    Описаны CRUD представления для модели продукта
    Создан шаблон главной страницы index.html
    
    Изменен базовый шаблон
    
    создан путь на главную страницу и страницу просмотра товара

--- review_app/urls.py
+++ review_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from review_app.views.base import ListView
+from review_app.views.base import IndexView, DisplayProductView
 
 
 urlpatterns = [
-    path("", ListView.as_view(), name='index')
+    path("", IndexView.as_view(), name='index_view'),
+    path("product/<int:pk>", DisplayProductView.as_view(), name='display_product')
 ]
--- review_app/views/base.py
+++ review_app/views/base.py
@@ -1,5 +1,34 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from review_app.models import Product
 
 
 class IndexView(ListView):
-    pass
+    template_name: str = 'index.html'
+    model = Product
+    context_object_name = 'products'
+    paginate_by = 5
+    paginate_orphans: int = 1
+
+
+class DisplayProductView(DetailView):
+    template_name: str = 'display_product.html'
+    model = Product
+
+
+class AddProductView(CreateView):
+    template_name: str = 'add_product.html'
+    model = Product
+    form_class = ProductForm
+    success_url = '/'
+
+class EditProductView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name: str = 'edit_product.html'
+    success_url = '/'
+
+
+class DeleteProductView(DeleteView):
+    model = Product
+    template_name: str = 'confirm_delete.html'
+    success_url = '/'
